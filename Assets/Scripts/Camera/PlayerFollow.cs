4943c15b274a2946e451511e3e8d52375a4e0a6c
Ramon Ferreira <ramon_felipe@outlook.com>
2021-07-30 23:11:40 -0300

    Adding camera movement

---
+++ Assets/Scripts/Camera/PlayerFollow.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    [SerializeField] Transform _playerTrasnform;
+    [SerializeField] Vector3 _cameraOffset;
+
+    [Range(0.1f, 1f)] 
+    [SerializeField] float _smoothFactor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - _playerTrasnform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPos = _playerTrasnform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, _smoothFactor);
+    }
+}
