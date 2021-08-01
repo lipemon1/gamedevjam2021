092c5bfe0704d1d927b9c58c1ea9053446d43af7
jsouza <jsouza@realtec.com.br>
2021-08-01 13:47:19 -0300

    Creating scene Credits

--- Assets/Scripts/UI/Menu/UIManager.cs
+++ Assets/Scripts/UI/Menu/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -16,7 +17,7 @@ public class UIManager : MonoBehaviour
 
     public void Credits()
     {
-
+        SceneManager.LoadScene("Credits");
     }
 
     public void Quit()
