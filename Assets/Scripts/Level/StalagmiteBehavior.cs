a316b50502e52668ccb57c4d41909267fca7a631
jsouza <jsouza@realtec.com.br>
2021-08-07 19:55:06 -0300

    Disable mesh renderer of trigger

--- Assets/Scripts/Level/StalagmiteBehavior.cs
+++ Assets/Scripts/Level/StalagmiteBehavior.cs
@@ -6,10 +6,14 @@ public class StalagmiteBehavior : MonoBehaviour
 {
 
     [SerializeField] GameObject _stalagmite;
+    MeshRenderer _meshRenderer;
 
     private void Awake()
     {
+        _meshRenderer = GetComponent<MeshRenderer>();
+
         _stalagmite.SetActive(false);
+        _meshRenderer.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
