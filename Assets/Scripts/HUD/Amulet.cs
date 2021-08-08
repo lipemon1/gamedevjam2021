5dc54dd8c0ebfa9109ba38109ef673ae1972c640
jsouza <jsouza@realtec.com.br>
2021-08-08 11:26:44 -0300

    Changing HUD assets

--- Assets/Scripts/HUD/Amulet.cs
+++ Assets/Scripts/HUD/Amulet.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Level;
 using UnityEngine;
 using Image = UnityEngine.UI.Image;
@@ -7,30 +8,38 @@ public class Amulet : MonoBehaviour
 {
     [SerializeField] int _numberOfLevels;
     [SerializeField] Image _amulet;
+    [SerializeField] List<Sprite> amuletList;
     static int _levelDone;
 
     void Awake()
     {
-        _levelDone = PlayerPrefs.GetInt("LevelDone");
+        _levelDone = PlayerPrefs.GetInt("LevelDone", 0);
     }
 
     // Start is called before the first frame update
     void Start()
     {
         LevelManager.Instance.OnLevelChange += OnLevelChange;
+
+        UpdateAmulet();
     }
 
     void OnLevelChange(string curlevel)
     {
         _levelDone++;
-        
+
         PlayerPrefs.SetInt("LevelDone", _levelDone);
-        
-        UpdateAmulet(_numberOfLevels);
+
+        UpdateAmulet();
     }
 
-    void UpdateAmulet(int maxAmount)
+    void UpdateAmulet()
     {
-        _amulet.fillAmount = PlayerPrefs.GetInt("LevelDone") / maxAmount;
+        int levelDone = PlayerPrefs.GetInt("LevelDone", 0);
+
+        if (levelDone > 8)
+            levelDone = 0;
+
+        _amulet.sprite = amuletList[levelDone];
     }
 }
