34ad59d90d26f1aeffbaf52dac6f62857b97136e
Ramon Ferreira <ramon_felipe@outlook.com>
2021-08-16 22:33:06 -0300

    Setting FMOD and Triggers

--- Assets/Scripts/Audio/BGMController.cs
+++ Assets/Scripts/Audio/BGMController.cs
@@ -1,5 +1,7 @@
+using System;
 using FMODUnity;
 using Level;
+using SoulSystem.FollowPlayer;
 using UnityEngine;
 using WorldChange;
 
@@ -7,6 +9,9 @@ namespace Audio
 {
     public class BGMController : MonoBehaviour
     {
+        [SerializeField] string _deadWorld;
+        [SerializeField] string _humanWorld;
+        [SerializeField] string _chasing;
         [SerializeField] StudioEventEmitter _emitter;
         void Awake()
         {
@@ -16,17 +21,48 @@ namespace Audio
         // Start is called before the first frame update
         void Start()
         {
+            _emitter.SetParameter(_deadWorld, 1f);
+            _emitter.Play();
+            
             WorldChangeManager.Instance.OnNewWorld += OnNewWorld;
             LevelManager.Instance.OnLevelChange += OnLevelChange;
+            FollowPlayerManager.Instance.OnFollowPlayer += OnFollowPlayer;
+            FollowPlayerManager.Instance.OnStopFollowPlayer += OnStopFollowPlayer;
+        }
+
+        void OnStopFollowPlayer(Transform player)
+        {
+            _emitter.SetParameter(_chasing, 0f);
+        }
+
+        void OnFollowPlayer(Transform player)
+        {
+            _emitter.SetParameter(_chasing, 1f);
         }
 
         void OnLevelChange(string curlevel)
         {
-            // throw new System.NotImplementedException();
+            if (curlevel == "Level 8")
+            {
+                // _emitter.SetParameter(_chasing, 1f);
+            }
         }
 
         void OnNewWorld(World newworld)
         {
+            switch (newworld)
+            {
+                case World.HumanWorld:
+                    _emitter.SetParameter(_deadWorld, 0f);
+                    _emitter.SetParameter(_humanWorld, 1f);
+                    break;
+                case World.SoulWorld:
+                    _emitter.SetParameter(_deadWorld, 1f);
+                    _emitter.SetParameter(_humanWorld, 0f);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(newworld), newworld, null);
+            }
             // throw new System.NotImplementedException();
         }
     }
