442c9ec5a131f7c0dc11837356e48e760d99e6b8
Ramon Ferreira <ramon_felipe@outlook.com>
2021-08-20 00:49:11 -0300

    Apply BGM

--- Assets/Scripts/Audio/BGMController.cs
+++ Assets/Scripts/Audio/BGMController.cs
@@ -9,12 +9,21 @@ namespace Audio
 {
     public class BGMController : MonoBehaviour
     {
+        public static BGMController Instance { get; set; }
+        
+        [SerializeField] string _normal;
         [SerializeField] string _deadWorld;
         [SerializeField] string _humanWorld;
         [SerializeField] string _chasing;
+        [SerializeField] string _level8;
         [SerializeField] StudioEventEmitter _emitter;
         void Awake()
         {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(this.gameObject);
+            
             DontDestroyOnLoad(this.gameObject);
         }
 
@@ -30,21 +39,35 @@ namespace Audio
             FollowPlayerManager.Instance.OnStopFollowPlayer += OnStopFollowPlayer;
         }
 
+        public void Reset()
+        {
+            _emitter.SetParameter(_level8, 0f);
+            _emitter.SetParameter(_chasing, 0f);
+            _emitter.SetParameter(_humanWorld, 0f);
+        }
+
         void OnStopFollowPlayer(Transform player)
         {
+            _emitter.SetParameter(_deadWorld, 1f);
             _emitter.SetParameter(_chasing, 0f);
         }
 
         void OnFollowPlayer(Transform player)
         {
+            _emitter.SetParameter(_deadWorld, 0f);
             _emitter.SetParameter(_chasing, 1f);
         }
 
         void OnLevelChange(string curlevel)
         {
+            if (curlevel == "Level 2")
+            {
+                _emitter.SetParameter(_normal, 1f);
+            }
+            
             if (curlevel == "Level 8")
             {
-                // _emitter.SetParameter(_chasing, 1f);
+                _emitter.SetParameter(_level8, 1f);
             }
         }
 
